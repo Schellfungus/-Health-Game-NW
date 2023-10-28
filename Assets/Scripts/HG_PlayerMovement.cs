5ce3d254d7443d91b7f882ed46cd3cbbfcfdd43f
Schellfungus <dschlossi1@gmail.com>
2023-10-28 23:06:17 +0200

    PlayerMovementBasics

--- Assets/Scripts/HG_PlayerMovement.cs
+++ Assets/Scripts/HG_PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class HG_PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    public Rigidbody player_Rigidbody;
+    public float movespeed, jumpForce;
+
+    private Vector2 moveInput;
+
+    public LayerMask palyer_WhatIsGround;
+    public Transform groundPoint;
+    private bool isGrounded;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.Normalize();
+
+        player_Rigidbody.velocity = new Vector3(moveInput.x * movespeed, player_Rigidbody.velocity.y, moveInput.y * movespeed);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundPoint.position, Vector3.down, out hit, 2f, palyer_WhatIsGround))
+        {
+            isGrounded = true;
+        }
+        else { isGrounded = false; }
+
+        if(Input.GetButtonDown("Jump") && isGrounded) 
+        {
+            player_Rigidbody.velocity += new Vector3(0f, jumpForce, 0f);
+        }
     }
 }
