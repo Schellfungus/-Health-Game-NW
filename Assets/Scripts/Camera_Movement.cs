a7a28ad3ee3313ad41d702f837b6d4116b8034e1
kyriuto <145133379+kyriuto@users.noreply.github.com>
2023-11-02 08:37:17 +0100

    stached Filles published

---
+++ Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement: MonoBehaviour
+{
+    public GameObject Dieter;
+    public float veraenderungX;
+    public float veraenderungY;
+    public float veraenderungZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void bewegungZuSpieler ()
+    //{
+      //  Dieter.getPosition();
+    //}
+}
